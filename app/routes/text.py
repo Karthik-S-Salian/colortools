f411e44c3b67bfa6bc076b9c64dd49d576adadb3
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-06-22 23:02:35 +0530

    deleting to migarte to astro

--- app/routes/text.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter,Request
-from ..template import getTemplate
-from fastapi.responses import HTMLResponse
-
-router = APIRouter(prefix="/text", tags=["text"])
--- app/template.py
+++
@@ -1,7 +0,0 @@
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
-
-templates = Jinja2Templates(directory="templates")
-
-def getTemplate(path:str,request:Request,params:dict={}):
-    return templates.TemplateResponse(path,{"request":request,**params})
